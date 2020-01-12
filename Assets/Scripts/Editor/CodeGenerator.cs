ca30d7be674cb2f841778c75b6e695cc79bc6702
bad3p <sxl@list.ru>
2020-01-12 17:52:00 +0300

    F! Automated generation of C# structs based on provided compute shader structs

--- Assets/Scripts/Editor/CodeGenerator.cs
+++ Assets/Scripts/Editor/CodeGenerator.cs
@@ -18,6 +18,28 @@ public class CodeGenerator
         GenerateCSharpStructs( structSrcs );
     }
 
+    static int IndexOfSpaceOrTab(string str)
+    {
+        int indexOfSpace = str.IndexOf(" ");
+        int indexOfTab = str.IndexOf("\t");
+        if (indexOfSpace != -1 && indexOfTab == -1)
+        {
+            return indexOfSpace;
+        }
+        else if (indexOfSpace == -1 && indexOfTab != -1)
+        {
+            return indexOfTab;
+        }
+        else if (indexOfSpace != -1 && indexOfTab != -1)
+        {
+            return Mathf.Min(indexOfSpace, indexOfTab);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
     static List<KeyValuePair<string,string>> SplitStructs(string src)
     {
         var result = new List<KeyValuePair<string,string>>();
@@ -46,17 +68,17 @@ public class CodeGenerator
             string structHeader = src.Substring(substringIndex, openingBraceIndex - substringIndex);
             string structBody = src.Substring(openingBraceIndex, closingBraceIndex - openingBraceIndex + 1);
 
-            structHeader = structHeader.Trim(new char[]{' ', '\n'});
-            structBody = structBody.Trim(new char[] {' ', '\n'});
+            structHeader = structHeader.Trim(new char[]{' ', '\n', '\r'});
+            structBody = structBody.Trim(new char[] {' ', '\n', '\r'});
 
-            int spaceIndex = structHeader.IndexOf(" ");
+            int spaceIndex = IndexOfSpaceOrTab(structHeader);
             if (spaceIndex == -1)
             {
                 Debug.LogError( "[CodeGenerator] unable to parse struct header.");
                 break;
             }
 
-            while (spaceIndex < structHeader.Length && structHeader[spaceIndex] == ' ')
+            while (spaceIndex < structHeader.Length && (structHeader[spaceIndex] == ' ' || structHeader[spaceIndex] == '\t'))
             {
                 spaceIndex++;
             }
@@ -85,9 +107,107 @@ public class CodeGenerator
         text += "\n";
         text += "namespace Structs\n";
         text += "{\n";
+        text += "\n\n";
+
+        foreach (var structSrc in structSrcs)
+        {
+            string structName = structSrc.Key;
+            string structBody = structSrc.Value;
+
+            text += "public struct " + structName + "\n";
+            text += "{\n";
+
+            var fields = SplitFields( structBody );
+            foreach (var field in fields)
+            {
+                var fieldType = field.Key;
+                var fieldName = field.Value;
+
+                text += "    public ";
+                if (fieldType == "float")
+                {
+                    text += "float";
+                }
+                else if (fieldType == "int")
+                {
+                    text += "int";
+                }
+                else if (fieldType == "float2")
+                {
+                    text += "Vector2";
+                }
+                else if (fieldType == "int2")
+                {
+                    text += "Vector2Int";
+                }
+                else if (fieldType == "float3")
+                {
+                    text += "Vector3";
+                }
+                else if (fieldType == "int3")
+                {
+                    text += "Vector3Int";
+                }
+                text += " " + field.Value + ";\n";
+            }
+            
+            text += "};\n";
+            text += "\n\n";
+        }
+        
+        text += "\n\n";
         text += "}\n";
         
         System.IO.File.WriteAllText( CSharpStructsPath, text, Encoding.Default);
         AssetDatabase.Refresh();
+    }
+
+    static List<KeyValuePair<string,string>> SplitFields(string structBody)
+    {
+        var result = new List<KeyValuePair<string, string>>();
+
+        structBody = structBody.Trim(new char[] {' ', '\n', '\r', '{', '}'});
+        
+        int index = 0;
+        while (index < structBody.Length)
+        {
+            int semicolonIndex = structBody.IndexOf(";", index );
+            if (semicolonIndex == -1)
+            {
+                break;
+            }
+
+            string fieldBody = structBody.Substring(index, semicolonIndex - index);
+            fieldBody = fieldBody.Trim(new char[] {' ', '\n', '\r'});
+            
+            int startSpaceIndex = IndexOfSpaceOrTab( fieldBody );
+            if (startSpaceIndex == -1)
+            {
+                Debug.LogError( "[CodeGenerator] unable to parse field body.");
+                break;
+            }
+            
+            int endSpaceIndex = startSpaceIndex;
+            
+            while (endSpaceIndex < fieldBody.Length && (fieldBody[endSpaceIndex] == ' ' || fieldBody[endSpaceIndex] == '\t'))
+            {
+                endSpaceIndex++;
+            }
+
+            if (endSpaceIndex == fieldBody.Length)
+            {
+                Debug.LogError( "[CodeGenerator] unable to parse field body.");
+                break;
+            }
+            
+            result.Add( new KeyValuePair<string,string>( 
+                fieldBody.Substring(0, startSpaceIndex ),
+                fieldBody.Substring(endSpaceIndex, fieldBody.Length - endSpaceIndex )
+            ) );
+
+            index = semicolonIndex+1;
+        }
+        
+        return result;
     }
 }
--- Assets/Scripts/Simulation/Structs.cs
+++ Assets/Scripts/Simulation/Structs.cs
@@ -2,4 +2,53 @@ using UnityEngine;
 
 namespace Structs
 {
+
+
+public struct FirearmsData
+{
+    public Vector3Int distance;
+    public Vector3Int firepower;
+};
+
+
+public struct UnitData
+{
+    public int type;
+    public int maxPersonnel;
+    public int maxAmmo;
+    public int maxPrimaryFirearms;
+    public int maxSecondaryFirearms;
+    public int primaryFirearmsDataID;
+    public int secondaryFirearmsDataID;
+};
+
+
+public struct Unit
+{
+    public int unitDataID;
+    public int carrierUnitID;
+    public int ready;
+    public int wounded;
+    public int level;
+    public int experience;
+    public int morale;
+    public int fitness;
+    public int ammo;
+    public int primaryFirearms;
+    public int secondaryFirearms;
+    public Vector3 pos;
+    public Vector3 ext;
+    public Vector3 vel;
+};
+
+
+public struct Group
+{
+    public int hqID;
+    public Vector3Int subID;
+};
+
+
+
+
 }
